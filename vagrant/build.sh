1dc34693b66e0ebe1c5c178a35cd959287fcaeb1
Douglas <douglasulian@hotmail.com>
2017-10-05 13:21:42 -0300

    Add douglas user to VMs, set ssh correctly

--- vagrant/build.sh
+++ vagrant/build.sh
@@ -1,6 +1,9 @@
+vagrant destroy -f
 vagrant up
 cat Vagrantfile | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | while read line
-do     
+do
   echo "Removing offending ECSA key in $HOME/.ssh/known_hosts for $line"
   ssh-keygen -f "$HOME/.ssh/known_hosts" -R "$line"
+
+  ssh douglas@$line -o StrictHostKeyChecking=no exit
 done
