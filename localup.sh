2455c2dcebed347b6a77e8d798da271e1473bde6
Douglas <douglasulian@hotmail.com>
2017-10-11 19:40:41 -0300

    Adjusted vpn scripts and postgresql firewall

--- localup.sh
+++ localup.sh
@@ -1,4 +1,4 @@
 sudo route del -net 10.238.4.0 netmask 255.255.255.0 dev tun1
-sudo bsvpndown
+rbsvpndown
 sudo route add -net 10.238.4.0 netmask 255.255.255.0 dev vboxnet1
 sudo route add -net 10.240.68.0 netmask 255.255.255.0 dev vboxnet2
